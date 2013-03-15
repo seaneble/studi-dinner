fae14ee13182bb1c6accdd1ca3db6e932fdb14df
sebasti@nleitz.de <sebasti@nleitz.de@6eccd330-d201-8e5a-a7c6-47c44e4812e6>
2013-03-15 16:28:04 +0000

    Kommentar-Header

--- trunk/application/models/Team.php
+++ trunk/application/models/Team.php
@@ -1,8 +1,18 @@
 <?php
 
+namespace application\models;
+
 /**
+ * Team-Datensatz
+ * 
+ * Eine lange Beschreibung
+ * 
  * @Entity
  * @Table(name="team")
+ * 
+ * @copyright 2013 DHBW StuV Stuttgart
+ * @author    Sebastian Leitz <webmaster@stuv-stuttgart.de>
+ * @version   $Id$
  */
 class Team
 {
