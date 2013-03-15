ed8d38d581980406ed919390d442b201e7395126
benjamin.mannal@gmail.com <benjamin.mannal@gmail.com@6eccd330-d201-8e5a-a7c6-47c44e4812e6>
2013-03-15 16:35:55 +0000

    Event Klasse hinzugefügt

---
+++ application/models/Event.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+* @Entity
+* @Table(name="event")
+*/
+class Event
+{
+	/**
+	 * Eindeutige laufende Nummer eines Events
+	 * 
+	 * @Id @Column(type="integer")
+	 * @GeneratedValue(strategy="IDENTITY")
+	 *
+	 * @var int
+	 */
+	private $id;
+	
+	/**
+	 * @Column(type="string", length=255)
+	 *
+	 * @var string
+	 */
+	private $name;
+	
+	/**
+	 * @Column /type="date"
+	 * 
+	 * @var DateTime
+	 */
+	private $date;
+}
